a0052b8eb6f6884aa6067fd0e9e2dbabd1e7e72c
owlesia <leself40@gmail.com>
2023-02-18 09:43:58 -0600

    set px4 git tag

--- src/lib/version/px_update_git_header.py
+++ src/lib/version/px_update_git_header.py
@@ -6,14 +6,27 @@ import sys
 import subprocess
 import re
 
-parser = argparse.ArgumentParser(description="""Extract version info from git and
+parser = argparse.ArgumentParser(
+    description="""Extract version info from git and
 generate a version header file. The working directory is expected to be
-the root of Firmware.""")
-parser.add_argument('filename', metavar='version.h', help='Header output file')
-parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
-                    help='Verbose output', default=False)
-parser.add_argument('--validate', dest='validate', action='store_true',
-                    help='Validate the tag format', default=False)
+the root of Firmware."""
+)
+parser.add_argument("filename", metavar="version.h", help="Header output file")
+parser.add_argument(
+    "-v",
+    "--verbose",
+    dest="verbose",
+    action="store_true",
+    help="Verbose output",
+    default=False,
+)
+parser.add_argument(
+    "--validate",
+    dest="validate",
+    action="store_true",
+    help="Validate the tag format",
+    default=False,
+)
 
 args = parser.parse_args()
 filename = args.filename
@@ -21,10 +34,10 @@ verbose = args.verbose
 validate = args.validate
 
 try:
-    fp_header = open(filename, 'r')
+    fp_header = open(filename, "r")
     old_header = fp_header.read()
 except:
-    old_header = ''
+    old_header = ""
 
 
 # Generate the header file content
@@ -36,27 +49,36 @@ header = """
 
 
 # PX4
-git_describe_cmd = 'git describe --exclude ext/* --always --tags --dirty'
-git_tag = subprocess.check_output(git_describe_cmd.split(),
-                                  stderr=subprocess.STDOUT).decode('utf-8').strip()
+git_describe_cmd = "git describe --exclude ext/* --always --tags --dirty"
+# git_tag = subprocess.check_output(git_describe_cmd.split(),
+#                                   stderr=subprocess.STDOUT).decode('utf-8').strip()
+git_tag = "v1.13.2"
 
 try:
     # get the tag if we're on a tagged commit
-    tag_or_branch = subprocess.check_output((git_describe_cmd+' --exact-match').split(),
-                                            stderr=subprocess.STDOUT).decode('utf-8').strip()
+    tag_or_branch = (
+        subprocess.check_output(
+            (git_describe_cmd + " --exact-match").split(), stderr=subprocess.STDOUT
+        )
+        .decode("utf-8")
+        .strip()
+    )
 except:
     tag_or_branch = None
 
 if validate:
     if verbose:
-        print("testing git tag: "+git_tag)
+        print("testing git tag: " + git_tag)
     # remove optional '-dirty' at the end
-    git_tag_test = re.sub(r'-dirty$', '', git_tag)
+    git_tag_test = re.sub(r"-dirty$", "", git_tag)
     # remove optional -<num_commits>-g<commit_hash> at the end (in case we are not on a tagged commit)
-    git_tag_test = re.sub(r'-[0-9]+-g[0-9a-fA-F]+$', '', git_tag_test)
+    git_tag_test = re.sub(r"-[0-9]+-g[0-9a-fA-F]+$", "", git_tag_test)
     # now check the version format
-    m = re.match(r'v([0-9]+)\.([0-9]+)\.[0-9]+(((-dev)|(-alpha[0-9]+)|(-beta[0-9]+)|(-rc[0-9]+))|'\
-                 r'(-[0-9]+\.[0-9]+\.[0-9]+((-dev)|(-alpha[0-9]+)|(-beta[0-9]+)|([-]?rc[0-9]+))?))?$', git_tag_test)
+    m = re.match(
+        r"v([0-9]+)\.([0-9]+)\.[0-9]+(((-dev)|(-alpha[0-9]+)|(-beta[0-9]+)|(-rc[0-9]+))|"
+        r"(-[0-9]+\.[0-9]+\.[0-9]+((-dev)|(-alpha[0-9]+)|(-beta[0-9]+)|([-]?rc[0-9]+))?))?$",
+        git_tag_test,
+    )
     if m:
         # format matches, check the major and minor numbers
         major = int(m.group(1))
@@ -69,43 +91,68 @@ if validate:
             sys.exit(1)
     else:
         print("")
-        print("Error: the git tag '{:}' does not match the expected format.".format(git_tag_test))
+        print(
+            "Error: the git tag '{:}' does not match the expected format.".format(
+                git_tag_test
+            )
+        )
         print("")
         print("The expected format is 'v<PX4 version>[-<custom version>]'")
-        print("  <PX4 version>: v<major>.<minor>.<patch>[-rc<rc>|-beta<beta>|-alpha<alpha>|-dev]")
-        print("  <custom version>: <major>.<minor>.<patch>[-rc<rc>|-beta<beta>|-alpha<alpha>|-dev]")
+        print(
+            "  <PX4 version>: v<major>.<minor>.<patch>[-rc<rc>|-beta<beta>|-alpha<alpha>|-dev]"
+        )
+        print(
+            "  <custom version>: <major>.<minor>.<patch>[-rc<rc>|-beta<beta>|-alpha<alpha>|-dev]"
+        )
         print("Examples:")
         print("  v1.9.0-rc3 (preferred)")
         print("  v1.9.0-beta1")
         print("  v1.9.0-1.0.0")
         print("  v1.9.0-1.0.0-alpha2")
-        print("See also https://dev.px4.io/master/en/setup/building_px4.html#firmware_version")
+        print(
+            "See also https://dev.px4.io/master/en/setup/building_px4.html#firmware_version"
+        )
         print("")
         sys.exit(1)
 
-git_version = subprocess.check_output('git rev-parse --verify HEAD'.split(),
-                                      stderr=subprocess.STDOUT).decode('utf-8').strip()
+git_version = (
+    subprocess.check_output(
+        "git rev-parse --verify HEAD".split(), stderr=subprocess.STDOUT
+    )
+    .decode("utf-8")
+    .strip()
+)
 try:
-    git_branch_name = subprocess.check_output('git symbolic-ref -q --short HEAD'.split(),
-                                          stderr=subprocess.STDOUT).decode('utf-8').strip()
+    git_branch_name = (
+        subprocess.check_output(
+            "git symbolic-ref -q --short HEAD".split(), stderr=subprocess.STDOUT
+        )
+        .decode("utf-8")
+        .strip()
+    )
 except:
-    git_branch_name = ''
+    git_branch_name = ""
 git_version_short = git_version[0:16]
 
 # OEM version
 try:
-    oem_tag = subprocess.check_output('git describe --match ext/oem-* --tags'.split(),
-                                      stderr=subprocess.STDOUT).decode('utf-8').strip()
+    oem_tag = (
+        subprocess.check_output(
+            "git describe --match ext/oem-* --tags".split(), stderr=subprocess.STDOUT
+        )
+        .decode("utf-8")
+        .strip()
+    )
     oem_tag = oem_tag[8:]
 except:
-    oem_tag = ''
+    oem_tag = ""
 
 if tag_or_branch is None:
     # replace / so it can be used as directory name
-    tag_or_branch = git_branch_name.replace('/', '-')
+    tag_or_branch = git_branch_name.replace("/", "-")
     # either a release or master branch (used for metadata)
-    if not tag_or_branch.startswith('release-'):
-        tag_or_branch = 'master'
+    if not tag_or_branch.startswith("release-"):
+        tag_or_branch = "master"
 
 header += f"""
 #define PX4_GIT_VERSION_STR "{git_version}"
@@ -120,12 +167,22 @@ header += f"""
 
 
 # ECL
-if (os.path.exists('src/lib/ecl/.git')):
-    ecl_git_tag = subprocess.check_output('git describe --always --tags --dirty'.split(),
-                                  cwd='src/lib/ecl', stderr=subprocess.STDOUT).decode('utf-8')
+if os.path.exists("src/lib/ecl/.git"):
+    ecl_git_tag = subprocess.check_output(
+        "git describe --always --tags --dirty".split(),
+        cwd="src/lib/ecl",
+        stderr=subprocess.STDOUT,
+    ).decode("utf-8")
 
-    ecl_git_version = subprocess.check_output('git rev-parse --verify HEAD'.split(),
-                                      cwd='src/lib/ecl', stderr=subprocess.STDOUT).decode('utf-8').strip()
+    ecl_git_version = (
+        subprocess.check_output(
+            "git rev-parse --verify HEAD".split(),
+            cwd="src/lib/ecl",
+            stderr=subprocess.STDOUT,
+        )
+        .decode("utf-8")
+        .strip()
+    )
     ecl_git_version_short = ecl_git_version[0:16]
 
     header += f"""
@@ -135,9 +192,16 @@ if (os.path.exists('src/lib/ecl/.git')):
 
 
 # Mavlink
-if (os.path.exists('src/modules/mavlink/mavlink/.git')):
-    mavlink_git_version = subprocess.check_output('git rev-parse --verify HEAD'.split(),
-                                      cwd='src/modules/mavlink/mavlink', stderr=subprocess.STDOUT).decode('utf-8').strip()
+if os.path.exists("src/modules/mavlink/mavlink/.git"):
+    mavlink_git_version = (
+        subprocess.check_output(
+            "git rev-parse --verify HEAD".split(),
+            cwd="src/modules/mavlink/mavlink",
+            stderr=subprocess.STDOUT,
+        )
+        .decode("utf-8")
+        .strip()
+    )
     mavlink_git_version_short = mavlink_git_version[0:16]
 
     header += f"""
@@ -147,13 +211,29 @@ if (os.path.exists('src/modules/mavlink/mavlink/.git')):
 
 
 # NuttX
-if (os.path.exists('platforms/nuttx/NuttX/nuttx/.git')):
-    nuttx_git_tags = subprocess.check_output('git -c versionsort.suffix=- tag --sort=v:refname'.split(),
-                                  cwd='platforms/nuttx/NuttX/nuttx', stderr=subprocess.STDOUT).decode('utf-8').strip()
-    nuttx_git_tag = re.findall(r'nuttx-[0-9]+\.[0-9]+\.[0-9]+', nuttx_git_tags)[-1].replace("nuttx-", "v")
-    nuttx_git_tag = re.sub('-.*', '.0', nuttx_git_tag)
-    nuttx_git_version = subprocess.check_output('git rev-parse --verify HEAD'.split(),
-                                      cwd='platforms/nuttx/NuttX/nuttx', stderr=subprocess.STDOUT).decode('utf-8').strip()
+if os.path.exists("platforms/nuttx/NuttX/nuttx/.git"):
+    nuttx_git_tags = (
+        subprocess.check_output(
+            "git -c versionsort.suffix=- tag --sort=v:refname".split(),
+            cwd="platforms/nuttx/NuttX/nuttx",
+            stderr=subprocess.STDOUT,
+        )
+        .decode("utf-8")
+        .strip()
+    )
+    nuttx_git_tag = re.findall(r"nuttx-[0-9]+\.[0-9]+\.[0-9]+", nuttx_git_tags)[
+        -1
+    ].replace("nuttx-", "v")
+    nuttx_git_tag = re.sub("-.*", ".0", nuttx_git_tag)
+    nuttx_git_version = (
+        subprocess.check_output(
+            "git rev-parse --verify HEAD".split(),
+            cwd="platforms/nuttx/NuttX/nuttx",
+            stderr=subprocess.STDOUT,
+        )
+        .decode("utf-8")
+        .strip()
+    )
     nuttx_git_version_short = nuttx_git_version[0:16]
 
     header += f"""
@@ -165,6 +245,6 @@ if (os.path.exists('platforms/nuttx/NuttX/nuttx/.git')):
 
 if old_header != header:
     if verbose:
-        print('Updating header {}'.format(filename))
-    fp_header = open(filename, 'w')
+        print("Updating header {}".format(filename))
+    fp_header = open(filename, "w")
     fp_header.write(header)
